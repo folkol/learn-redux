eb45d0f1b3d2cab376dce39f2d96efa2b00103c8
Matte <mattias4@kth.se>
2017-08-26 22:39:58 +0200

    Now connecting components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
 
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
@@ -11,12 +11,14 @@ import { Provider } from 'react-redux';
 import store, { history } from './store';
 
 const router = (
-	<Router history={browserHistory}>
-		<Router path="/" component={Main}>
-			<IndexRoute component={PhotoGrid}></IndexRoute>
-			<Route path="/view/:postId" component={Single}></Route>
+	<Provider store={store}>
+		<Router history={history}>
+			<Router path="/" component={App}>
+				<IndexRoute component={PhotoGrid}></IndexRoute>
+				<Route path="/view/:postId" component={Single}></Route>
+			</Router>
 		</Router>
-	</Router>
+	</Provider>
 );
 
 render(router, document.getElementById('root'));
