96d94e69696969e2cc16e65ecbeaba233b854fc0
Matte <mattias4@kth.se>
2017-08-27 01:19:46 +0200

    Added hot deploy of reducers

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -25,4 +25,12 @@ const store = createStore(
 );
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if(module.hot) {
+	module.hot.accept('./reducers', () => {
+		const nextRootReducer = require('./reducers/index').default;
+		store.replaceReducer(nextRootReducer);
+	});
+}
+
 export default store;
